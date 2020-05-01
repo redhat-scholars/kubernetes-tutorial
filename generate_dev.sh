a2a053a0a9e627456170b1d6cf8cb7f30078a013
yanaga <edson@yanaga.io>
2020-05-01 17:27:58 -0400

    Remove unused antora yaml files and scripts

--- generate_dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run -v `pwd`:/antora --rm -t antora/antora:2.2.0 --pull --stacktrace dev-site.yml
--- staging.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-_CURR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-rm -rf ./gh-pages .cache
-
-antora --pull --stacktrace  staging.yaml
