783009d733f9082922ebaddc614e596c84565680
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2021-06-30 01:45:10 +0000

    Update for use with minikube clusters running locally
    example usage: '/workspaces/kubernetes-tutorial/scripts/create-kubeconfig.sh' localhost "" "marc.hildenbrand" devnation

--- scripts/create-kubeconfig.sh
+++ scripts/create-kubeconfig.sh
@@ -10,7 +10,7 @@ declare KEYFILE_PATH=${2:-${HOME}/.ssh/emu-fedora}
 # user on the minikube host
 declare USER=${3:-mwh}
 
-declare MINIKUBE_CTX_NAME=${4:-minikube}
+declare MINIKUBE_PROFILE_NAME=${4:-minikube}
 
 # as the remote server, assume we want the kubeconfig at the exported KUBECONFIG location
 declare REMOTE_KUBECONFIG_PATH=${KUBECONFIG}
@@ -28,27 +28,46 @@ if [[ ! -d ${CERTS_DIR} ]]; then
 fi
 
 # Assume this is run right after minikube is setup on host machine for user ${USER}
-scp -i ${KEYFILE_PATH} ${USER}@${MINIKUBE_HOST}:~/.kube/config ${REMOTE_KUBECONFIG_PATH}
+if [[ $MINIKUBE_HOST == "localhost" ]]; then
+    cp ~/.kube/config ${REMOTE_KUBECONFIG_PATH}
+else
+    scp -i ${KEYFILE_PATH} ${USER}@${MINIKUBE_HOST}:~/.kube/config ${REMOTE_KUBECONFIG_PATH}
+fi
 
 # find the host directory for certs.  For kubectl config documentation and examples, see
 # this site: https://kubernetes.io/docs/reference/kubectl/cheatsheet/#kubectl-context-and-configuration
-CA_CRT=$(kubectl config view -o jsonpath="{.clusters[?(@.name == \"${MINIKUBE_CTX_NAME}\")].cluster.certificate-authority}" --kubeconfig=${REMOTE_KUBECONFIG_PATH})
-kubectl config set clusters.${MINIKUBE_CTX_NAME}.certificate-authority "${CERTS_DIR}/$(basename ${CA_CRT})" --kubeconfig=${REMOTE_KUBECONFIG_PATH}
-CLIENT_CRT=$(kubectl config view -o jsonpath="{.users[?(@.name == \"${MINIKUBE_CTX_NAME}\")].user.client-certificate}" --kubeconfig=${REMOTE_KUBECONFIG_PATH})
-kubectl config set users.${MINIKUBE_CTX_NAME}.client-certificate "${CERTS_DIR}/$(basename ${CLIENT_CRT})" --kubeconfig=${REMOTE_KUBECONFIG_PATH}
-CLIENT_KEY=$(kubectl config view -o jsonpath="{.users[?(@.name == \"${MINIKUBE_CTX_NAME}\")].user.client-key}" --kubeconfig=${REMOTE_KUBECONFIG_PATH})
-kubectl config set users.${MINIKUBE_CTX_NAME}.client-key "${CERTS_DIR}/$(basename ${CLIENT_KEY})" --kubeconfig=${REMOTE_KUBECONFIG_PATH}
+CA_CRT=$(kubectl config view -o jsonpath="{.clusters[?(@.name == \"${MINIKUBE_PROFILE_NAME}\")].cluster.certificate-authority}" --kubeconfig=${REMOTE_KUBECONFIG_PATH})
+kubectl config set clusters.${MINIKUBE_PROFILE_NAME}.certificate-authority "${CERTS_DIR}/$(basename ${CA_CRT})" --kubeconfig=${REMOTE_KUBECONFIG_PATH}
+CLIENT_CRT=$(kubectl config view -o jsonpath="{.users[?(@.name == \"${MINIKUBE_PROFILE_NAME}\")].user.client-certificate}" --kubeconfig=${REMOTE_KUBECONFIG_PATH})
+kubectl config set users.${MINIKUBE_PROFILE_NAME}.client-certificate "${CERTS_DIR}/$(basename ${CLIENT_CRT})" --kubeconfig=${REMOTE_KUBECONFIG_PATH}
+CLIENT_KEY=$(kubectl config view -o jsonpath="{.users[?(@.name == \"${MINIKUBE_PROFILE_NAME}\")].user.client-key}" --kubeconfig=${REMOTE_KUBECONFIG_PATH})
+kubectl config set users.${MINIKUBE_PROFILE_NAME}.client-key "${CERTS_DIR}/$(basename ${CLIENT_KEY})" --kubeconfig=${REMOTE_KUBECONFIG_PATH}
 
 declare FILES=( ${CA_CRT} ${CLIENT_CRT} ${CLIENT_KEY} )
 for HOST_FILE_PATH in ${FILES[@]}; do
     FILE_NAME=$(basename ${HOST_FILE_PATH})
     REMOTE_CERT_FILE_PATH="${CERTS_DIR}/${FILE_NAME}"
     
-    echo "Copying ${FILE_NAME} from ${MINIKUBE_HOST}:${HOST_FILE_PATH} to ${REMOTE_CERT_FILE_PATH}."
-    scp -i ${KEYFILE_PATH} ${USER}@${MINIKUBE_HOST}:${HOST_FILE_PATH} ${REMOTE_CERT_FILE_PATH}
+    if [[ $MINIKUBE_HOST == "localhost" ]]; then
+        LOCAL_CERT_FILE_PATH=~/.minikube/profiles/${MINIKUBE_PROFILE_NAME}/${FILE_NAME}
+        if [[ ! -f "$LOCAL_CERT_FILE_PATH" ]]; then
+            LOCAL_CERT_FILE_PATH=~/.minikube/${FILE_NAME}
+        fi       
+        
+        echo "Copying ${FILE_NAME} from ${LOCAL_CERT_FILE_PATH} to ${REMOTE_CERT_FILE_PATH}."
+    
+        cp "${LOCAL_CERT_FILE_PATH}" "${REMOTE_CERT_FILE_PATH}"
+    else
+        echo "Copying ${FILE_NAME} from ${MINIKUBE_HOST}:${HOST_FILE_PATH} to ${REMOTE_CERT_FILE_PATH}."
+
+        scp -i ${KEYFILE_PATH} ${USER}@${MINIKUBE_HOST}:${HOST_FILE_PATH} ${REMOTE_CERT_FILE_PATH}
+    fi
+    
 done
 
 
 
 # Reset the server on the config to the current host
-kubectl config set clusters.${MINIKUBE_CTX_NAME}.server "https://${MINIKUBE_HOST}:8443" --kubeconfig=${REMOTE_KUBECONFIG_PATH}  
+if [[ ${MINIKUBE_HOST} != "localhost" ]]; then
+    kubectl config set clusters.${MINIKUBE_PROFILE_NAME}.server "https://${MINIKUBE_HOST}:8443" --kubeconfig=${REMOTE_KUBECONFIG_PATH}  
+fi
